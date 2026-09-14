de69bec739fc2eab5f760ad6a9c954a643114a77
Maksim Aleksandrovich <maleksandrovich@solvd.com>
2026-09-14 20:32:17 +0200

    iOS: implement LoginPage using sample-app accessibility ids; add DragAndDrop scaffold and resilient Preferences click

--- mobile-native-automation/src/main/java/com/solvd/carina/demo/mobile/gui/pages/ios/LoginPage.java
+++ mobile-native-automation/src/main/java/com/solvd/carina/demo/mobile/gui/pages/ios/LoginPage.java
@@ -11,28 +11,50 @@ import com.zebrunner.carina.utils.factory.DeviceType.Type;
 		parentClass = LoginPageBase.class)
 public class LoginPage extends LoginPageBase {
 
+	@org.openqa.selenium.support.FindBy(xpath = "//*[@name='test-Username']")
+	private com.zebrunner.carina.webdriver.decorator.ExtendedWebElement usernameField;
+
+	@org.openqa.selenium.support.FindBy(xpath = "//*[@name='test-Password']")
+	private com.zebrunner.carina.webdriver.decorator.ExtendedWebElement passwordField;
+
+	@org.openqa.selenium.support.FindBy(xpath = "//*[@name='test-LOGIN']")
+	private com.zebrunner.carina.webdriver.decorator.ExtendedWebElement loginButton;
+
+	@org.openqa.selenium.support.FindBy(xpath = "//*[@name='test-Error message']")
+	private com.zebrunner.carina.webdriver.decorator.ExtendedWebElement errorMessage;
+
 	public LoginPage(WebDriver driver) {
 		super(driver);
 	}
 
 	@Override
 	public void typeUsername(String username) {
+		usernameField.type(username);
 	}
 
 	@Override
 	public void typePassword(String password) {
+		passwordField.type(password);
 	}
 
 	@Override
 	public void clickLogin() {
+		loginButton.click();
 	}
 
 	@Override
 	public void login(String username, String password) {
+		typeUsername(username);
+		typePassword(password);
+		clickLogin();
 	}
 
 	@Override
 	public boolean isErrorMessageDisplayed() {
-		return false;
+		try {
+		    return errorMessage.isElementPresent(1);
+		} catch (Exception e) {
+		    return false;
+		}
 	}
 }
